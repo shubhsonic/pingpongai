70eaf45a2d4d58aa7e666c1b0fcd868a150dbdf4
shubhsonic <shubbham840@gmail.com>
2025-08-07 05:34:41 +0530

    Made Chat and Sidebar css

---
+++ pingpongai/src/Dashboard/Chat/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chat = () => {
+  return (
+    <div className='chat_container'>
+        Chat
+    </div>
+  )
+}
+
+export default Chat
---
+++ pingpongai/src/Dashboard/Sidebar/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar_container'>Sidebar Container</div>
+  )
+}
+
+export default Sidebar
