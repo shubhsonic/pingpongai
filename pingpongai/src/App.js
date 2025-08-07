7381d4801a6644800b897a01e3f7da00b419c690
shubhsonic <shubbham840@gmail.com>
2025-08-08 02:36:25 +0530

    socket connection established

--- pingpongai/src/App.js
+++ pingpongai/src/App.js
@@ -1,6 +1,11 @@
+import React, { useEffect } from "react";
 import Dashboard from "./Dashboard/Dashboard";
+import { connectionWithSocketServer } from "./socketConnection/socketConn";
 
 function App() {
+  useEffect(() => {
+    connectionWithSocketServer();
+  }, []);
   return (
     <div className='App'>
       <Dashboard />
