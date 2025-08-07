7521e59142da65c87f6cf64443fce83df98e7711
shubhsonic <shubbham840@gmail.com>
2025-08-07 08:21:51 +0530

    added transition to text output

--- pingpongai/src/Dashboard/Chat/Message.js
+++ pingpongai/src/Dashboard/Chat/Message.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { GrUser } from 'react-icons/gr';
 import { FcMindMap } from 'react-icons/fc';
 
+
+
 const Message = ({ content, aiMessage, animate }) => {
   return (
     <div
     className='message_container'
-    style={{ background: aiMessage ? "rgb(247, 247, 248)" : "whitle"}}
+    style={{ background: aiMessage ? "rgba(235, 236, 240)" : "white"}}
     >
         <div className='message_avatar_container'>
             {aiMessage ? <FcMindMap /> : <GrUser />}
