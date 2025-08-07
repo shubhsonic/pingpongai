4a5452773c00d58c443132e80e708995e7d91e6d
shubhsonic <shubbham840@gmail.com>
2025-08-08 03:39:43 +0530

    added functionality for new input message

--- pingpongai/src/Dashboard/Chat/NewMessageInput.js
+++ pingpongai/src/Dashboard/Chat/NewMessageInput.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BsSend } from 'react-icons/bs'
+import { v4 as uuid } from "uuid";
 
 const NewMessageInput = () => {
+  const [content, setContent] = useState('');
+
+  const proceedMessage =() => {
+      const message = {
+        aiMessage: false,
+        content,
+        id: uuid(),
+        animate: false,
+      };
+
+      console.log(message);
+
+      // append this message to our local store
+
+      // send message to the server
+
+      setContent('');
+  };
+
+  // Reminder: what is message is "      " also add condition from trim()
+  const handleSendMessage = () => {
+    if(content.length > 0) {
+      proceedMessage();
+    }
+  }
+
+  const handleKeyPressed = (event) => {
+    if(event.code === "Enter" && content.length > 0) {
+      proceedMessage();
+    }
+  }
+
   return (
     <div className='new_message_input_container'>
         <input 
             className='new_message_input' 
             type="text" name="" 
             placeholder='Ask anything' 
-            value=''
-            onChange={()=>{}}
-            onKeyDown={()=>{}}
+            value={content}
+            onChange={(e)=>setContent(e.target.value)}
+            onKeyDown={handleKeyPressed}
         />
-        <div className='new_message_icon_container' onClick={()=>{}}>
+        <div className='new_message_icon_container' onClick={handleSendMessage}>
             <BsSend color='grey' />
         </div>
     </div>
